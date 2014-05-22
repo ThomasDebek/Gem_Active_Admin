da576a9efc7e31eb5b0abbda37da0493dafac92b
ThomasDebek <tom.debek@gmail.com>
2014-05-22 20:42:28 +0200

    ActiceAdmin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AppActiveadmin::Application.config.secret_key_base = '03a4623fb2f2360b614d370e9a972f5545c04bfb86e2bbfe5e16e6e89f3234ce32c89b1738e6d7c715944008d2fcee774f7174f13de8894001ff2f6bec9bb32c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AppActiveadmin::Application.config.session_store :cookie_store, key: '_app_activeadmin_session'
